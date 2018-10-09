0d31fbf072bc84c0239d4e95a5e3073c52d7cb47
Jonathan Toole-Charignon <13107869+jtoolec@users.noreply.github.com>
2018-10-09 11:35:36 +0200

    Add initial class files for trams and stations

---
+++ station.py
@@ -0,0 +1,8 @@
+class Station:
+    def __init__(self, plat_1_busy=False, plat_2_busy=False):
+        self.plat_1_busy = plat_1_busy
+        self.plat_2_busy = plat_2_busy
+
+    def gen_wait():
+        pass
+        
---
+++ tram.py
@@ -0,0 +1,12 @@
+class Tram:
+    def __init__(self, schedule, location=0, passengers=0):
+        self.schedule = schedule
+        self.location = location
+        self.passengers = passengers
+
+    def event_arrival():
+        pass
+
+    def event_departure():
+        pass
+        
